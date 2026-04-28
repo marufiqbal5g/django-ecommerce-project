7d8959dff01bed1730c062fe46fc7c24e3afd3d0
marufiqbal5g <kmiqbal1989@gmail.com>
2026-04-28 17:58:44 +0600

    CRUP func only operates by superuser

--- store/api_views.py
+++ store/api_views.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .permissions import IsSuperUserOrReadOnly
+
 from .models import Product, Order, OrderItem, Customer, ShippingAddress
 from .serializers import (
     ProductSerializer,
@@ -11,20 +14,24 @@ from .serializers import (
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsSuperUserOrReadOnly]
 
 class ShippingAddressViewSet(viewsets.ModelViewSet):
     queryset = ShippingAddress.objects.all()
     serializer_class = ShippingAddressSerializer
-
+    permission_classes = [IsSuperUserOrReadOnly]
